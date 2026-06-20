a89dadb16849f902e7037171946557d2ea41d485
Ahtisham khan Mughal <09-131242-014@student.bahria.edu.pk>
2026-06-20 23:35:37 +0500

    feat: initialize backend server with express and basic routing

--- 01-todo-app/to-do-app/backend/server.js
+++ 01-todo-app/to-do-app/backend/server.js
@@ -4,10 +4,17 @@ const express = require("express");
 const app=express();
 
 //middlewares
-
+app.use(express.json());
 
 //routes
+app.get('/test',(req,res)=>{
+  res.status(200).send('<h1>Welcome to To-do App MERN Node Server </h1>');
+})
 
 //port
+const PORT=8080
 
 //listen
+app.listen(PORT,()=>{
+  console.log('Server is Running on Port',PORT);
+})
