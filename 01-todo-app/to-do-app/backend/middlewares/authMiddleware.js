0a61bc86c138867e9c2bbeda274d2eb1b2f5113b
Ahtisham khan Mughal <09-131242-014@student.bahria.edu.pk>
2026-06-26 03:12:56 +0500

    feat: implement JWT authentication middleware and integrate it into todo creation route

---
+++ 01-todo-app/to-do-app/backend/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+const JWT = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ success: false, message: "auth middleware error" });
+      } else {
+        req.body.id = decode.id;
+        next();
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .send({
+        success: false,
+        message: "auth middleware error",
+        error: err.message,
+      });
+  }
+};
--- 01-todo-app/to-do-app/backend/routes/todoRouter.js
+++ 01-todo-app/to-do-app/backend/routes/todoRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { createtodoController } = require('../controllers/todoController');
-
+const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 //create todo
-router.post('/create',createtodoController);
+router.post('/create', authMiddleware, createtodoController);
 
 //export
 module.exports = router;  
