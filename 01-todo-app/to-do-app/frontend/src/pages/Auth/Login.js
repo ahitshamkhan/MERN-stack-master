0591c1beeb540f34199ab4e4036aa13e4551902d
Ahtisham khan Mughal <09-131242-014@student.bahria.edu.pk>
2026-06-25 16:09:40 +0500

    feat: implement authentication pages with login and registration forms

--- 01-todo-app/to-do-app/frontend/src/pages/Auth/Login.js
+++ 01-todo-app/to-do-app/frontend/src/pages/Auth/Login.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React,{useState}  from "react";
+import { Link} from "react-router-dom";
+import "./AuthStyles.css";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login Page</h2>
-            
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const loginHandler=(e)=>{
+    try {
+      e.preventDefault();
+      alert("Login Data Email: "+ email +" Password: " + password);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className="form-container">
+      <div className="form">
+        <div className="mb-3">
+          <i className="fa-solid fa-circle-user"></i>
         </div>
-    )
-}
+        <div className="mb-3">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="enter email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="enter password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-bottom">
+          <p className="text-center">
+            not a user? please
+            <Link to="/register"> Regiser</Link>
+          </p>
+          < button type="submit" className="login-btn" onClick={loginHandler} >LOGIN </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login; 
+export default Login;
--- 01-todo-app/to-do-app/frontend/src/pages/Auth/Register.js
+++ 01-todo-app/to-do-app/frontend/src/pages/Auth/Register.js
@@ -1,12 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./AuthStyles.css";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>Register Page</h2>
-            
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
-export default Register; 
+  const registerHandler = (e) => {
+    try {
+      e.preventDefault();
+      alert("Register Data Email: " + email + " Password: " + password + " Username: " + username); 
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <div className="form">
+        <div className="mb-3">
+          <i className="fa-solid fa-circle-user"></i>
+        </div>
+         <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-bottom">
+          <p className="text-center">
+            Already a user? please
+            <Link to="/login"> Login</Link>
+          </p>
+          <button type="submit" className="login-btn" onClick={registerHandler}>
+            REGISTER{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
