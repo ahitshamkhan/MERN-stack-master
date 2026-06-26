09efde495e0112aedc88d820fb93ef1d38c8f689
Ahtisham khan Mughal <09-131242-014@student.bahria.edu.pk>
2026-06-27 04:01:18 +0500

    feat: implement task creation modal and integration in home page

--- 01-todo-app/to-do-app/frontend/src/pages/Home/HomePage.js
+++ 01-todo-app/to-do-app/frontend/src/pages/Home/HomePage.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import Navbar from "../../components/Layout/Navbar";
+import PopModel from "../../components/Layout/PopModel";
+
 const Home = () => {
+  const [showModal,setShowModal]=useState(false);
+  const [title,setTitle]=useState("");
+  const [description,setDescription]=useState("");
+
+
+  const openhandleShowModal=()=>{
+    setShowModal(true);
+  } 
   return (
     <div>
       <Navbar />
@@ -8,10 +18,17 @@ const Home = () => {
         <div className="add-task">
           <h1>Your Task</h1>
           <input type="search" placeholder="search your task" />
-          <button className=" btn btn-primary">
+          <button className=" btn btn-primary" onClick={openhandleShowModal}>
             Create Task <i className="fa-solid fa-plus"></i>
           </button>
         </div>
+        <PopModel
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title={title}
+        setTitle={setTitle}
+        description={description}
+        setDescription={setDescription}/>
       </div>
     </div>
   );
